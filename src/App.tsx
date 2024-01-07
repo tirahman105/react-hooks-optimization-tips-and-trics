947bb45ee672e7ae2bb55ac5f7d7b90770a4deef
tirahman105 <tirahman105@gmail.com>
2024-01-07 23:30:18 +0600

    UseState Batch Update And Async Behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import UseStateExample from "./pages/UseStateExample";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  // const handleIncrement = () => {
+  //   setCounter((prevState) => prevState + 1);
+  // };
+
+  // const handleAsyncIncrement = () => {
+  //   setTimeout(() => {
+  //     setCounter((prevState) => prevState + 1);
+  //   }, 2000);
+  // };
   return (
     <div className="App">
-      <UseStateExample></UseStateExample>
+      <UseStateExample
+        counter={counter}
+        setCounter={setCounter}
+      ></UseStateExample>
     </div>
   );
 }
--- src/pages/UseStateExample.tsx
+++ src/pages/UseStateExample.tsx
@@ -1,22 +1,28 @@
-import React, { useState } from "react";
-const UseStateExample = () => {
-  const [counter, setCounter] = useState(0);
+type TCounter = {
+  counter: number;
+  setCounter: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const UseStateExample = ({ counter, setCounter }: TCounter) => {
   return (
     <div className="border p-10 shadow-md">
       <h1 className="text-3xl">Counter</h1>
       <h1 className="text-3xl">{counter}</h1>
       <button
         className="btn btn-primary "
-        onClick={() => setCounter((prvStatus) => prvStatus + 1)}
+        onClick={() => setCounter(counter + 1)}
       >
         Increment
+      </button>
+      {/* <button className="btn-primary " onClick={handleAsyncIncrement}>
+        Async Increment
       </button>
       <button
         className="btn-primary "
         onClick={() => setCounter((prvStatus) => prvStatus - 1)}
       >
         Decrement
-      </button>
+      </button> */}
       <button className="btn-primary" onClick={() => setCounter(0)}>
         Reset
       </button>
